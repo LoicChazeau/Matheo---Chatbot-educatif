111ba3551158d005b43e05b84305cb014ffd4d59
Loïc Chazeau <loic.chazeau@gmail.com>
2024-06-05 18:56:00 +0200

    WIP chat history

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -9,21 +9,86 @@
       <span>Nouvelle discussion</span>
     </div>
     <div class="chat-list">
-      <div class="no-history">
+      <div v-if="!hasChats" class="no-history">
         Aucun historique, vous pouvez commencer une nouvelle discussion !
       </div>
-      <div class="chat-list-separator">
-        <div class="separator"></div>
-        <span class="separator-text">Aujourd'hui</span>
-        <div class="separator"></div>
-      </div>
-      <div class="chat-list-separator">
-        <div class="separator"></div>
-        <span class="separator-text">Hier</span>
-        <div class="separator"></div>
-      </div>
-      <div class="chat-item" v-for="chat in chats" :key="chat.id">
-        <div @click="selectChat(chat)">{{ chat.name }}</div>
+      <div v-else>
+        <div v-if="this.todayChats.length" class="chat-list-separator">
+          <div class="separator"></div>
+          <span class="separator-text">Aujourd'hui</span>
+          <div class="separator"></div>
+        </div>
+        <div
+          class="chat-item"
+          v-for="chat in todayChats"
+          :key="chat.id"
+          @click="selectChat(chat)"
+        >
+          <img src="../assets/chat.svg" alt="chat icon" />
+          <span>{{ chat.name }}</span>
+          <img
+            class="chat-item-option"
+            src="../assets/option.svg"
+            alt="chat options"
+          />
+        </div>
+        <div v-if="yesterdayChats.length" class="chat-list-separator">
+          <div class="separator"></div>
+          <span class="separator-text">Hier</span>
+          <div class="separator"></div>
+        </div>
+        <div
+          class="chat-item"
+          v-for="chat in yesterdayChats"
+          :key="chat.id"
+          @click="selectChat(chat)"
+        >
+          <img src="../assets/chat.svg" alt="chat icon" />
+          <span>{{ chat.name }}</span>
+          <img
+            class="chat-item-option"
+            src="../assets/option.svg"
+            alt="chat options"
+          />
+        </div>
+        <div v-if="last7DaysChats.length" class="chat-list-separator">
+          <div class="separator"></div>
+          <span class="separator-text">7 jours précédents</span>
+          <div class="separator"></div>
+        </div>
+        <div
+          class="chat-item"
+          v-for="chat in last7DaysChats"
+          :key="chat.id"
+          @click="selectChat(chat)"
+        >
+          <img src="../assets/chat.svg" alt="chat icon" />
+          <span>{{ chat.name }}</span>
+          <img
+            class="chat-item-option"
+            src="../assets/option.svg"
+            alt="chat options"
+          />
+        </div>
+        <div v-if="olderChats.length" class="chat-list-separator">
+          <div class="separator"></div>
+          <span class="separator-text">Anciennes conversations</span>
+          <div class="separator"></div>
+        </div>
+        <div
+          class="chat-item"
+          v-for="chat in olderChats"
+          :key="chat.id"
+          @click="selectChat(chat)"
+        >
+          <img src="../assets/chat.svg" alt="chat icon" />
+          <span>{{ chat.name }}</span>
+          <img
+            class="chat-item-option"
+            src="../assets/option.svg"
+            alt="chat options"
+          />
+        </div>
       </div>
     </div>
     <div class="footer">
@@ -40,26 +105,95 @@
 </template>
 
 <script>
+import { collection, query, where, getDocs } from "firebase/firestore";
+
 export default {
   props: ["chats"],
+  data() {
+    return {
+      todayChats: [],
+      yesterdayChats: [],
+      last7DaysChats: [],
+      olderChats: [],
+      userINE: JSON.parse(localStorage.getItem("user")).ine,
+    };
+  },
+  computed: {
+    hasChats() {
+      return (
+        this.todayChats.length ||
+        this.yesterdayChats.length ||
+        this.last7DaysChats.length ||
+        this.olderChats.length
+      );
+    },
+  },
+  async created() {
+    await this.fetchConversations();
+  },
   methods: {
+    async fetchConversations() {
+      const convRef = collection(this.$db, "conversations");
+      const q = query(convRef, where("user_id", "==", this.userINE));
+      const querySnapshot = await getDocs(q);
+
+      const today = new Date();
+      const yesterday = new Date(today);
+      yesterday.setDate(today.getDate() - 1);
+      const last7Days = new Date(today);
+      last7Days.setDate(today.getDate() - 7);
+
+      const conversations = [];
+
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        data.id = doc.id;
+        data.last_used = new Date(data.last_used.seconds * 1000);
+        conversations.push(data);
+      });
+
+      console.log("Conversations :", conversations);
+
+      this.todayChats = conversations.filter((chat) =>
+        this.isSameDay(chat.last_used, today)
+      );
+      this.yesterdayChats = conversations.filter((chat) =>
+        this.isSameDay(chat.last_used, yesterday)
+      );
+      this.last7DaysChats = conversations.filter((chat) => {
+        const chatDate = new Date(chat.last_used);
+        return chatDate > last7Days && chatDate < yesterday;
+      });
+      this.olderChats = conversations.filter(
+        (chat) => new Date(chat.last_used) <= last7Days
+      );
+    },
+    isSameDay(d1, d2) {
+      console.log(d1, d2);
+      return (
+        d1.getFullYear() === d2.getFullYear() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getDate() === d2.getDate()
+      );
+    },
     createNewChat() {
       // Logique pour créer un nouveau chat
     },
     selectChat(chat) {
       // Logique pour sélectionner un chat
     },
-		logout() {
-      localStorage.removeItem('user');
-      this.$router.push('/');
-    }
+    logout() {
+      localStorage.removeItem("user"); // Supprime les informations de l'utilisateur du localStorage
+      this.$router.push("/"); // Redirige vers la page de connexion
+    },
   },
 };
 </script>
 
 <style scoped>
 .sidebar {
-  max-width: 260px;
+  width: 260px;
+  min-width: 260px;
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -119,6 +253,33 @@ export default {
   margin: 0 15px;
   font-size: 14px;
   font-weight: 300;
+  white-space: nowrap;
+}
+.chat-item {
+  background-color: transparent;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 10px;
+  padding: 12px 0;
+  height: 24px;
+  border-radius: 12px;
+  cursor: pointer;
+}
+.chat-item:hover {
+  background-color: #2b2e2f;
+}
+.chat-item img {
+  margin-left: 15px;
+  margin-right: 10px;
+}
+.chat-item span {
+  flex-grow: 1;
+  font-size: 0.85rem;
+}
+.chat-item-option {
+  margin-left: 0px;
+  margin-right: 15px;
 }
 .footer {
   margin: 20px 0;
